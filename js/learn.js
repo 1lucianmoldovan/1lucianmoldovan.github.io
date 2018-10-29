f1e3ddc294923cdeffcb895c571fa37e18f61567
Lucian Moldovan <43965995+1lucianmoldovan@users.noreply.github.com>
2018-10-29 20:27:00 +0200

    variabile

---
+++ js/learn.js
@@ -0,0 +1,18 @@
+console.info(0);
+
+var nume = "Moldovan";
+var prenume = "Lucian";
+var owner = "Lucian's";
+var msg1 = 'Ai zis cumva "Luci"';
+var msg2 = 'Nu, am zis "Lucian"';
+var template = `Text cu ghilimele " sau simple'`;
+
+console.debug("auzi.. ? dar cum te cheama ?!");
+
+var skills = ["Microsoft Office Tools", "HTML", "JS", "Oracle ERP"];
+
+prenume = "Lucas";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
