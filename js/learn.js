5fea7a08cabf320b6c69dcd516a94f6f3fc5bd98
Lucian Moldovan <43965995+1lucianmoldovan@users.noreply.github.com>
2018-11-05 21:22:20 +0200

    doua versiuni for & while

--- js/learn.js
+++ js/learn.js
@@ -1,56 +1,122 @@
-console.info(0);
+function LearnVariables() {
+  function sayWelcome() {
+    console.info("Salut " + prenume + ", bine ai venit la noi!");
+  }
 
-function sayWelcome() {
-  console.info("Salut " + prenume + ", bine ai venit la noi!");
+  var nume = "Moldovan";
+  var prenume = "Lucian";
+  var owner = "Lucian's";
+  var msg1 = 'Ai zis cumva "Luci"';
+  var msg2 = 'Nu, am zis "Lucian"';
+  var template = `Text cu ghilimele " sau simple'`;
+
+  console.debug("auzi.. ? dar cum te cheama ?!");
+  sayWelcome();
+
+  var skills = ["Microsoft Office Tools", "HTML", "JS", "Oracle ERP"];
+
+  prenume = "Lucas";
+
+  console.info("toate variabilele au fost initializate");
+  console.log(nume);
+  console.debug("cum te cheama? -" + prenume);
+  sayWelcome();
 }
 
-var nume = "Moldovan";
-var prenume = "Lucian";
-var owner = "Lucian's";
-var msg1 = 'Ai zis cumva "Luci"';
-var msg2 = 'Nu, am zis "Lucian"';
-var template = `Text cu ghilimele " sau simple'`;
+function learnIterators() {
+  function countTo(n) {
+    console.info("sa numaram pana la " + n);
+    //for(init; condition; after) scris in doua variante
+    for (var i = 1; i <= n; i++) {
+      console.info(i);
+    }
 
-console.debug("auzi.. ? dar cum te cheama ?!");
-sayWelcome();
+    var i = 1;
+    for (; i <= n; ) {
+      console.info(i);
+      i++;
+    }
+  }
 
-var skills = ["Microsoft Office Tools", "HTML", "JS", "Oracle ERP"];
+  function countDown(n) {
+    var i = n;
+    for (; i > 0; ) {
+      console.info(i);
+      i--;
+    }
 
-prenume = "Lucas";
+    var i = n;
+    while (i > 0) {
+      console.info(i);
+      i--;
+    }
+  }
 
-console.info("toate variabilele au fost initializate");
-console.log(nume);
-console.debug("cum te cheama? -" + prenume);
-sayWelcome();
+  countTo(10);
+  countDown(10);
+}
+
+learnIterators();
+
+// LearnVariables ();
 
 //schimb valutar
 
-var cursEUR = 4.5;
-var myEUR = 20;
-var myRON = 0;
-var friendEUR = 10;
-var friendRON = 0;
-// operatori * / + -
+// var cursEUR = 4.5;
+// var myEUR = 20;
+// var myRON = 0;
+// var friendEUR = 10;
+// var friendRON = 0;
+// // operatori * / + -
 
-myRON = myEUR * cursEUR;
-console.info("Eu am " + myRON + " RON");
+// myRON = myEUR * cursEUR;
+// console.info("Eu am " + myRON + " RON");
 
-friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
-console.info("Tu ai " + friendRON + " RON");
+// friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
+// console.info("Tu ai " + friendRON + " RON");
 
-function extractFromATM(ammount) {
-  console.info("==== ==== ==== ==== ====");
-  console.info("suma extrasa este: " + ammount);
+var ATMFunds = 50000;
+var funds = 5000;
+
+function getExtractFee(ammount) {
   var comision = ammount * 0.01;
   if (comision < 2.5) {
     comision = 2.5;
     console.warn("comision minim aplicat");
   }
+  return comision;
+}
+// TODO implement
+function checkExtractPermissions() {
+  return true;
+}
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ====");
+  console.info("suma extrasa este: " + ammount);
+  var comision = getExtractFee(ammount);
+
+  var totalExtract = ammount + comision;
+
+  if (totalExtract > funds) {
+    console.error("insufficient funds");
+    return;
+  }
+  if (ATMFunds < totalExtract) {
+    console.error("ATM insufficient funds");
+    return;
+  }
+
+  funds = funds - totalExtract;
+  // ATMFunds = ATMFunds - ammount;
+  ATMFunds -= ammount;
   console.info("comision aplicat: " + comision);
+  console.info("sold curent: " + funds);
   console.info("==== ==== ==== ==== ====");
 }
 
-extractFromATM(1000);
-extractFromATM(100);
-
-var x = 4 + 5;
+// extractFromATM(1005); // nu e multiplu de 10
+// extractFromATM(100000); // solduri insuficiente
+// extractFromATM(3000);
+// extractFromATM(3000);
+// extractFromATM(100);
