bc9169b1bf2a4984282be0d19a9a660304a47dee
LucianMoldovan <1lucianmoldovan@gmail.com>
2018-11-19 21:23:42 +0200

    hideAllPages & show current page

--- src/index.js
+++ src/index.js
@@ -10,38 +10,25 @@ function show(id) {
 
 }
 
+//$("home-menu").onclick = showHomePage;
+//$("skills-menu").onclick = showSkillsPage;
+//$("education-menu").onclick = showEducationPage;
+//$("languages-menu").onclick = showLanguagesPage;
 
-function showHomePage() {
-  hide("skills-page");
-  hide("languages-page");
-  show("home-page");
-  hide("education-page");
+function hideAllPages() {
+  var pages = document.querySelectorAll(".page-block");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = 'none';
+  }
 }
 
-function showSkillsPage() {
-  hide("home-page");
-  hide("languages-page");
-  show("skills-page");
-  hide("education-page");
+var links = document.querySelectorAll("#top-menu-bar a");
+for (var i = 0; i < links.length; i++) {
+  links[i].onclick = function (){
+    hideAllPages();
+    var page = this.getAttribute('data-page');
+    show(page + "-page");
+  };
 }
 
-function showEducationPage() {
-  hide("home-page");
-  hide("languages-page");
-  show("education-page");
-  hide("skills-page");
-
-}
-
-function showLanguagesPage() {
-  hide("home-page");
-  hide("skills-page");
-  show("languages-page");
-  hide("education-page");
-
-}
-
-$("home-menu").onclick = showHomePage;
-$("skills-menu").onclick = showSkillsPage;
-$("education-menu").onclick = showEducationPage;
-$("languages-menu").onclick = showLanguagesPage;
+show("home-page");
