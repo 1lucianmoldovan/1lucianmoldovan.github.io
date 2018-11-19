4fa6227c5d65f7460c2097e00de226fe14422c3c
LucianMoldovan <1lucianmoldovan@gmail.com>
2018-11-19 19:53:57 +0200

    add education page

--- src/index.js
+++ src/index.js
@@ -15,20 +15,33 @@ function showHomePage() {
   hide("skills-page");
   hide("languages-page");
   show("home-page");
+  hide("education-page");
 }
 
 function showSkillsPage() {
   hide("home-page");
   hide("languages-page");
   show("skills-page");
+  hide("education-page");
+}
+
+function showEducationPage() {
+  hide("home-page");
+  hide("languages-page");
+  show("education-page");
+  hide("skills-page");
+
 }
 
 function showLanguagesPage() {
   hide("home-page");
   hide("skills-page");
   show("languages-page");
+  hide("education-page");
+
 }
 
 $("home-menu").onclick = showHomePage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationPage;
 $("languages-menu").onclick = showLanguagesPage;
