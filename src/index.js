a5f145c3dd272f20baf4ae94f17b7a767ff653e5
LucianMoldovan <1lucianmoldovan@gmail.com>
2018-11-26 21:43:01 +0200

    map & join pe lista skills

--- src/index.js
+++ src/index.js
@@ -25,13 +25,31 @@ function hideAllPages() {
 
 }
 
-var links = document.querySelectorAll("#top-menu-bar a");
-for (var i = 0; i < links.length; i++) {
-  links[i].onclick = function (){
-    hideAllPages();
-    var page = this.getAttribute('data-page');
-    show(page + "-page");
-  };
+function initMenu() {
+  var links = document.querySelectorAll("#top-menu-bar a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+      hideAllPages();
+      var page = this.getAttribute('data-page');
+      show(page + "-page");
+    };
+  }
 }
 
-show("home-page");
+function initSkillsPage() {
+  var skills = ['Microsoft Office Tools', 'HTML', 'JS', 'Oracle ERP'];
+  var resultList = document.querySelector('#skills-page ul');
+
+var skillsLi = skills.map(function(skill) {
+  return `<li>${skill.toUpperCase()}</li>`;
+});
+
+
+  console.log('resultList:', skillsLi)
+  resultList.innerHTML = skillsLi.join('');
+}
+
+initMenu();
+show("skills-page");
+initSkillsPage();
+
