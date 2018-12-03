f2a53fc0e5ba125daff07e2475f24e42d3e84960
LucianMoldovan <1lucianmoldovan@gmail.com>
2018-12-03 21:08:35 +0200

    use jquery

--- src/index.js
+++ src/index.js
@@ -1,65 +1,39 @@
-function $(id) {
-  return document.getElementById(id);
-}
-function hide(id) {
-  $(id).style.display = "none";
-
-}
-function show(id) {
-  $(id).style.display = "block";
-
-}
-
-function hideElement(el) {
-  el.style.display = 'none';
-}
-
-//$("home-menu").onclick = showHomePage;
-//$("skills-menu").onclick = showSkillsPage;
-//$("education-menu").onclick = showEducationPage;
-//$("languages-menu").onclick = showLanguagesPage;
-
-function hideAllPages() {
-  var pages = document.querySelectorAll(".page-block");
-  pages.forEach(hideElement);
-
-}
-
 function initMenu() {
   var links = document.querySelectorAll("#top-menu-bar a");
   for (var i = 0; i < links.length; i++) {
     links[i].onclick = function () {
-      hideAllPages();
+      $('.page-block').hide()
       var page = this.getAttribute('data-page');
       show(page + "-page");
+      $('#' + page + "-page").show();
     };
   }
 }
 
 function initSkillsPage() {
   var skills = [
-    {name: 'MICROSOFT OFFICE TOOLS', endorsments : 7, EndorsedBy: 'Andrei'},
-    {name: 'HTML', endorsments : 6, EndorsedBy: 'Cristian'},
-    {name: 'JS', endorsments : 2, EndorsedBy: 'Matei'},
-    {name: 'Oracle ERP', endorsments : 1, EndorsedBy: ''}
+    {name: 'MICROSOFT OFFICE TOOLS', endorsments : 7, endorsedBy: 'Andrei'},
+    {name: 'HTML', endorsments : 6, endorsedBy: 'Cristian'},
+    {name: 'JS', endorsments : 2, endorsedBy: 'Matei'},
+    {name: 'Oracle ERP', endorsments : 1, endorsedBy: ''}
   ];
   var resultList = document.querySelector('#skills-page ul');
 
 var listItems = skills.map(function(skill) {
-  var EndorsedBy = ' - Endorsed by'
-  if (skill.EndorsedBy == "") {
-    EndorsedBy = "";
+  var endorsedBy = ' - Endorsed by'
+  if (skill.endorsedBy == "") {
+    endorsedBy = "";
   }
   var name = skill.name.toUpperCase();
   return `<li>${name}
-  <span style="color: gray">- ${skill.endorsments} ${EndorsedBy}</span> 
-  ${skill.EndorsedBy}
+  <span style="color: gray">- ${skill.endorsments} ${endorsedBy}</span> 
+  ${skill.endorsedBy}
   </li>`;
 });
   resultList.innerHTML = listItems.join('');
 }
 
 initMenu();
-show("skills-page");
+$('#skills-page').show();
 initSkillsPage();
 
